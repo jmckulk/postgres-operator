cb462f4d8541c0d9f35f5202d9835401b1bfcd56
Chris Bandy <bandy.chris@gmail.com>
2022-01-25 23:15:12 -0600

    Bump go.opentelemetry.io/otel to v1.2.0
    A number of APIs changed along the path to v1. The latest is v1.3.0 but
    requires go-logr v1.
    
    Also switch to the OTLP exporter rather than Jaeger.

--- internal/controller/postgrescluster/instance.go
+++ internal/controller/postgrescluster/instance.go
@@ -24,7 +24,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	attributes "go.opentelemetry.io/otel/label"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -806,10 +806,10 @@ func (r *Reconciler) rolloutInstances(
 	}
 
 	span.SetAttributes(
-		attributes.Int("instances", len(instances.forCluster)),
-		attributes.Int("specified", numSpecified),
-		attributes.Int("available", numAvailable),
-		attributes.Int("considering", len(consider)),
+		attribute.Int("instances", len(instances.forCluster)),
+		attribute.Int("specified", numSpecified),
+		attribute.Int("available", numAvailable),
+		attribute.Int("considering", len(consider)),
 	)
 
 	// Redeploy instances up to the allowed maximum while "rolling over" any
